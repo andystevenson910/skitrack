0d2aa56a7a7e62441233a04b5173e77319296a74
Andrew Stevenson <andystevenson910@gmail.com>
2023-04-30 15:43:59 -0600

    got file uploads working

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: false,
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+  },
+};
